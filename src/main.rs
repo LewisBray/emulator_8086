b51e836b0f89d6eb8281d0dae065cacdad1aa00a
Lewis Bray <32199493+LewisBray@users.noreply.github.com>
2023-03-08 23:11:12 +0000

    Implemented reg-expression mov

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,21 @@ use std::env;
 use std::fs;
 
 const MOV_BITS: u8 = 0x88;
-const MOV_REG_FIELD_ENCODINGS: &'static [&str] = &[
+
+const REG_FIELD_ENCODINGS: &'static [&str] = &[
     "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh", // w = 0
     "ax", "cx", "dx", "bx", "sp", "bp", "si", "di"  // w = 1
+];
+
+const REG_EXPRESSION_ENCODINGS: &'static [&str] = &[
+    "bx + si",
+    "bx + di",
+    "bp + si",
+    "bp + di",
+    "si",
+    "di",
+    "bp",
+    "bx"
 ];
 
 fn main() {
@@ -18,29 +30,112 @@ fn main() {
     while byte_index < byte_count {
         let byte: u8 = bytes[byte_index];
         if byte & MOV_BITS != 0 {
-            let d_bit: u8 = (byte & 0x2) >> 1;
-            let w_bit: u8 = byte & 0x1;
+            let d_bit: u8 = (byte & 0x2) >> 1;  // 1 <=> reg field gives destination
+            let w_bit: u8 = byte & 0x1;         // 1 <=> wide version of instruction
             
             byte_index += 1;
             let byte: u8 = bytes[byte_index];
 
             let mod_field: u8 = (byte & 0xC0) >> 6;
-            debug_assert!(mod_field == 0x03);   // only handling reg to reg mov atm
-
             let reg_field: u8 = (byte & 0x38) >> 3;
             let rm_field: u8 = byte & 0x07;
 
-            let source_index: usize = ((1 - d_bit) * reg_field + d_bit * rm_field + 8 * w_bit) as usize;
-            debug_assert!(source_index < MOV_REG_FIELD_ENCODINGS.len());
-            let destination_index: usize = (d_bit * reg_field + (1 - d_bit) * rm_field + 8 * w_bit) as usize;
-            debug_assert!(destination_index < MOV_REG_FIELD_ENCODINGS.len());
+            match mod_field {
+                0x00 => {
+                    let expression_index: usize = rm_field as usize;
+                    debug_assert!(expression_index < REG_EXPRESSION_ENCODINGS.len());
+                    let field_index: usize = (reg_field + 8 * w_bit) as usize;
+                    debug_assert!(field_index < REG_FIELD_ENCODINGS.len());
 
-            let source: &str = MOV_REG_FIELD_ENCODINGS[source_index];
-            let destination: &str = MOV_REG_FIELD_ENCODINGS[destination_index];
+                    let expression: &str = REG_EXPRESSION_ENCODINGS[expression_index];
+                    let field: &str = REG_FIELD_ENCODINGS[field_index];
 
-            println!("mov {}, {}", destination, source);
+                    if d_bit == 1 {
+                        println!("mov {}, [{}]", field, expression);
+                    } else {
+                        println!("mov [{}], {}", expression, field);
+                    }
 
-            byte_index += 1;
+                    byte_index += 1;
+                },
+                0x01 => {
+                    let expression_index: usize = rm_field as usize;
+                    debug_assert!(expression_index < REG_EXPRESSION_ENCODINGS.len());
+                    let field_index: usize = (reg_field + 8 * w_bit) as usize;
+                    debug_assert!(field_index < REG_FIELD_ENCODINGS.len());
+
+                    let expression: &str = REG_EXPRESSION_ENCODINGS[expression_index];
+                    let field: &str = REG_FIELD_ENCODINGS[field_index];
+
+                    byte_index += 1;
+                    let displacement: u8 = bytes[byte_index];
+
+                    if d_bit == 1 {
+                        if displacement != 0 {
+                            println!("mov {}, [{} + {}]", field, expression, displacement);
+                        } else {
+                            println!("mov {}, [{}]", field, expression);
+                        }
+                    } else {
+                        if displacement != 0 {
+                            println!("mov [{} + {}], {}", expression, displacement, field);
+                        } else {
+                            println!("mov [{}], {}", expression, field);
+                        }
+                    }
+
+                    byte_index += 1;
+                },
+                0x02 => {
+                    let expression_index: usize = rm_field as usize;
+                    debug_assert!(expression_index < REG_EXPRESSION_ENCODINGS.len());
+                    let field_index: usize = (reg_field + 8 * w_bit) as usize;
+                    debug_assert!(field_index < REG_FIELD_ENCODINGS.len());
+
+                    let expression: &str = REG_EXPRESSION_ENCODINGS[expression_index];
+                    let field: &str = REG_FIELD_ENCODINGS[field_index];
+
+                    byte_index += 1;
+                    let displacement_low: u8 = bytes[byte_index];
+
+                    byte_index += 1;
+                    let displacement_high: u8 = bytes[byte_index];
+
+                    let displacement: u16 = ((displacement_high as u16) << 8) + displacement_low as u16;
+
+                    if d_bit == 1 {
+                        if displacement != 0 {
+                            println!("mov {}, [{} + {}]", field, expression, displacement);
+                        } else {
+                            println!("mov {}, [{}]", field, expression);
+                        }
+                    } else {
+                        if displacement != 0 {
+                            println!("mov [{} + {}], {}", expression, displacement, field);
+                        } else {
+                            println!("mov [{}], {}", expression, field);
+                        }
+                    }
+
+                    byte_index += 1;
+                },
+                0x03 => {
+                    let source_index: usize = ((1 - d_bit) * reg_field + d_bit * rm_field + 8 * w_bit) as usize;
+                    debug_assert!(source_index < REG_FIELD_ENCODINGS.len());
+                    let destination_index: usize = (d_bit * reg_field + (1 - d_bit) * rm_field + 8 * w_bit) as usize;
+                    debug_assert!(destination_index < REG_FIELD_ENCODINGS.len());
+
+                    let source: &str = REG_FIELD_ENCODINGS[source_index];
+                    let destination: &str = REG_FIELD_ENCODINGS[destination_index];
+
+                    println!("mov {}, {}", destination, source);
+                    
+                    byte_index += 1;
+                }
+                _ => {
+                    debug_assert!(false);
+                }
+            }
         } else {
             debug_assert!(false);   // Not handling any other instructions atm
         }
